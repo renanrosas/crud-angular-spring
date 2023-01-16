b4969d6904a7e8aa27138f72f337ba157eaae706
Renan Ramalho Rosas <renan-rosas@hotmail.com>
2023-01-16 20:18:03 -0300

    Courses service

--- crud-angular/src/app/courses/courses/courses.component.ts
+++ crud-angular/src/app/courses/courses/courses.component.ts
@@ -1,20 +1,28 @@
-import { Course } from './../models/course';
-import { Component } from '@angular/core';
+import { CoursesService } from './../services/courses.service';
+import { Course } from '../models/course';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
   styleUrls: ['./courses.component.scss']
 })
-export class CoursesComponent {
+export class CoursesComponent implements OnInit{
 
-  courses: Course[] = [
-    {_id: '1', name: 'Angular', category: 'Front-End'}
-  ];
+  courses: Observable<Course[]>;
   displayedColumns = ['name','category'];
 
-  constructor(){
+  //coursesService: CoursesService;
+
+  constructor(private coursesService: CoursesService){
     //this.courses = [];
+    //this.coursesService = new CoursesService();
+    this.courses = this.coursesService.list();
+
   }
 
+  ngOnInit(): void {
+
+  }
 }
---
+++ crud-angular/src/app/courses/services/courses.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { Course } from '../models/course';
+import { tap } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CoursesService {
+
+  private readonly API = '/assets/courses.json';
+
+  constructor(private httpClient: HttpClient) { }
+
+  list() {
+    return this.httpClient.get<Course[]>(this.API)
+    .pipe(
+      tap(courses => console.log(courses))
+    );
+  }
+}
