844c60b05e158533593a4a7632caaa16442cd54d
Renan Rosas <renan-rosas@hotmail.com>
2023-01-17 08:43:09 -0300

    add project spring

---
+++ crud-spring/src/test/java/com/renan/crudspring/CrudSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.renan.crudspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
