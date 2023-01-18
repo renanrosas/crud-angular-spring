3b080dcfb3f908f32fe9e57f5632df29452386cc
Renan Rosas <renan-rosas@hotmail.com>
2023-01-17 21:32:24 -0300

    H2 db configuration

--- crud-spring/src/main/java/com/renan/crudspring/model/Course.java
+++ crud-spring/src/main/java/com/renan/crudspring/model/Course.java
@@ -1,24 +1,25 @@
 package com.renan.crudspring.model;
 
-import org.springframework.data.annotation.Id;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
-import jakarta.persistence.Table;
+import jakarta.persistence.Id;
 import lombok.Data;
 
 @Data
 @Entity
-@Table(name = "courses")
+//@Table(name = "courses")
 public class Course {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @JsonProperty("_id")
     private Long id;
 
-    @Column(length = 200, nullable = false)
+    @Column(length = 100, nullable = false)
     private String name;
 
     @Column(length = 10, nullable = false)
