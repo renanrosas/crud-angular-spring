8a2fc3d685e7b1df5ff9ceb9224bf16c109c2d21
Renan Rosas <renan-rosas@hotmail.com>
2023-01-19 21:08:57 -0300

    Form css

--- crud-angular/src/app/courses/course-form/course-form.component.ts
+++ crud-angular/src/app/courses/course-form/course-form.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-course-form',
   templateUrl: './course-form.component.html',
-  styleUrls: ['./course-form.component.css']
+  styleUrls: ['./course-form.component.scss']
 })
 export class CourseFormComponent {
 
+  form: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
+
+    this.form = this.formBuilder.group({
+      name: [null],
+      category: [null]
+    });
+  }
+
+  onSubmit(){
+
+  }
+
+  onCancel(){
+
+  }
 }
